5de271afbb791f866e1b370960d8ec7e3edfa129
Thien Hai <hai@tgl-sol.com>
2020-11-23 21:33:39 -0800

    Updates

---
+++ precache-manifest.2d1b78416f900d52870e26f0c50256ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e651f8cf8418c4d985a3c06e79aaad0d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f8f9a6200f21d86c3c76",
+    "url": "./static/css/main.f026bbec.chunk.css"
+  },
+  {
+    "revision": "ed5f1cd0da15f7b7ec37",
+    "url": "./static/js/2.dee219e4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.dee219e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8f9a6200f21d86c3c76",
+    "url": "./static/js/main.4157c942.chunk.js"
+  },
+  {
+    "revision": "07138d46289adc0ddcfa",
+    "url": "./static/js/runtime-main.4dcd8c3e.js"
+  }
+]);
